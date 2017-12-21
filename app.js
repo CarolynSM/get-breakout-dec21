baed6ccbd7dd838d449fc1673c53c7318f425a64
Carolyn Selheim <carolynselheim@Carolyns-MacBook-Pro.local>
2017-12-21 11:18:00 -0700

    updates

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var getUrl = "";
 var button = document.querySelector("#button");
+var div = document.querySelector("#response");
 
-
-button.addEventListener("click", function(event) {
+button.addEventListener("click", function() {
   console.log("don't click me there");
 });
 
 
 // fetch(getUrl)
 //   .then(response => {
+//     console.log(response);
 //     return response.json();
 //   })
 //   .then(response => {
 //     console.log(response);
 //     // whatever you want to do with your responsed data
+//   })
+//   .catch(error => {
+//     console.log(error);
 //   });
