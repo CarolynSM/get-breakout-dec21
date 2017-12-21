cca95357dafeb9313cf615e95a115db9b239b095
Carolyn Selheim <carolynselheim@Carolyns-MacBook-Pro.local>
2017-12-21 11:06:00 -0700

    breakout frontend

---
+++ app.js
@@ -0,0 +1,17 @@
+var getUrl = "";
+var button = document.querySelector("#button");
+
+
+button.addEventListener("click", function(event) {
+  console.log("don't click me there");
+});
+
+
+// fetch(getUrl)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(response => {
+//     console.log(response);
+//     // whatever you want to do with your responsed data
+//   });
